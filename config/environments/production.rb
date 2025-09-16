2f5447283a99f69ffd7429c98a9fb83ed3cd3a90
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-09-16 13:35:27 +0530

    port as env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: ENV["GMAIL_SMTP_PORT"],
     domain: "gmail.com",
     user_name: ENV["GMAIL_USERNAME"], # e.g. "apikey" for SendGrid
     password: ENV["GMAIL_APP_PASSWORD"],
