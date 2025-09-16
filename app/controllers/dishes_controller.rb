17e711a5405d1b9f241d42910441ca2837edbe93
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-09-16 20:21:28 +0530

    require login to create dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,6 @@
 class DishesController < ApplicationController
   before_action :set_dish, only: %i[ show update destroy ]
-  #before_action :require_login, only: %i[new create]
+  before_action :require_login, only: %i[new create]
 
   # GET /dishes
   def index
