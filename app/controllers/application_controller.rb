b53e83fcb1adaa3990b1f92ce0f0fabdf2d41025
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-09-18 22:20:54 +0530

    increase session persistence to 3 hrs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :user_logged_in?
   before_action :check_session_timeout
 
-  SESSION_TIMEOUT = 30.minutes
+  SESSION_TIMEOUT = 3.hours
 
   private
 
