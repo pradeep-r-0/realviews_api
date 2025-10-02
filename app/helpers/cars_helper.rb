215fab08769c802c5e1b6f53672c924a7ee2fc39
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-02 11:33:48 +0530

    Handle mileage of new car with no fuel topups

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,8 +1,11 @@
 module CarsHelper
   def calculate_avg_mileage(fuel_topups)
+    return -1 unless fuel_topups.present?
+
     now = fuel_topups.last
     previous = fuel_topups.first
     return -1 unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
+    
     (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/fuel_topups.pluck(:price).sum.to_f)*previous.rate_per_litre.to_f).round(2)
   end
 end
