b4f3aeba332de24945686f8e21805c956477be28
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-03 21:28:32 +0530

    car validation and index width

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,8 @@ class Car < ApplicationRecord
   has_many :ownerships
   has_many :users, through: :ownerships
   has_many :fuel_topups, dependent: :destroy
+
+  validates :variant, :fuel_type, presence: true
+  # Composite uniqueness validation
+  validates :variant, uniqueness: { scope: [:car_make_id, :fuel_type], message: "with this make and fuel type already exists" }
 end
