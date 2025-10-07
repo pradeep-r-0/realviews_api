ccf9810b78133fee839432fb3f3bccb5497fe707
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-07 22:12:25 +0530

    bind 0 and min_threads_count

--- config/puma.rb
+++ config/puma.rb
@@ -24,11 +24,13 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 3 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT"){3000}}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
