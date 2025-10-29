5bae30659738e2bf9d0eac517faa6222db5dec04
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-30 00:28:14 +0530

    add name index to cities

---
+++ db/migrate/20251029184431_add_index_on_name_to_cities.rb
@@ -0,0 +1,5 @@
+class AddIndexOnNameToCities < ActiveRecord::Migration[8.0]
+  def change
+    add_index :cities, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_155037) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_29_184431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_17_155037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 1, null: false
+    t.index ["name"], name: "index_cities_on_name", unique: true
   end
 
   create_table "dishes", force: :cascade do |t|
