602ee65bf8b2da0e17d4fed72aea7326ae9a4da4
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-29 23:54:38 +0530

    safe sanitize restaurant name

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,13 @@
 class Restaurant < ApplicationRecord
   belongs_to :city
   has_many :dishes, dependent: :destroy
-  before_create :titleize_name
+  before_create :safe_titleize_name
 
   private
   
-  def titleize_name
-    self.name = name.to_s.titleize
+  def safe_titleize_name
+    self.name = self.name.split(/(\s*-\s*)/).map { |part|
+      part.match?(/-\s*/) ? part : part.titleize
+    }.join
   end
 end
