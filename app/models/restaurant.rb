0d705835a9e64b422ccd6450c743a593e1cc53f7
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-10-30 15:00:07 +0530

    restaurant model uniqueness

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,8 @@ class Restaurant < ApplicationRecord
   belongs_to :city
   has_many :dishes, dependent: :destroy
   before_create :safe_titleize_name
-
+  validates :name, uniqueness: { scope: :city_id, case_sensitive: false }
+  
   private
   
   def safe_titleize_name
