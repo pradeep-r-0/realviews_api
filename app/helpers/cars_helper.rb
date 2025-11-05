f6dd5a1646cde7275f5ab3cac995b74a0659aef2
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-11-05 09:42:09 +0530

    order for calculating mileage

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,9 +1,9 @@
 module CarsHelper
   def calculate_avg_mileage(fuel_topups)
-    return -1 unless fuel_topups.present?
+    return -1 unless fuel_topups.size > 2
     
-    now = fuel_topups.last
-    previous = fuel_topups.first
+    now = fuel_topups.first
+    previous = fuel_topups.second
     return -1 unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
     
     (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/fuel_topups.pluck(:price).sum.to_f)*previous.rate_per_litre.to_f).round(2)
