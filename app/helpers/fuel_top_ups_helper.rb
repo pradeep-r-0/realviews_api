eb7c8889a5c9c4dd6d7dfccc95637578d504c938
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-11-05 10:48:25 +0530

    mileage calculation reorder

--- app/helpers/fuel_top_ups_helper.rb
+++ app/helpers/fuel_top_ups_helper.rb
@@ -1,5 +1,5 @@
 module FuelTopUpsHelper
-  def calculate_mileage(previous, now)
+  def calculate_mileage(now, previous)
     (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/previous.price.to_f)*previous.rate_per_litre.to_f).round(2)
   end
 end
