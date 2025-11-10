1686a585836c6902582f7ba2b9f10ec36563ff7b
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-11-11 00:23:31 +0530

    avg_mileage calculation for multiple topups

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -3,9 +3,11 @@ module CarsHelper
     return -1 unless fuel_topups.size > 2
     
     now = fuel_topups.first
-    previous = fuel_topups.second
+
+    # size 2 for two record comparision, otherwise, it is overall mileage
+    previous = fuel_topups.last
     return -1 unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
     
-    (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/fuel_topups.pluck(:price).sum.to_f)*previous.rate_per_litre.to_f).round(2)
+    (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/(fuel_topups.pluck(:price) - [now.price]).sum.to_f)*previous.rate_per_litre.to_f).round(2)
   end
 end
