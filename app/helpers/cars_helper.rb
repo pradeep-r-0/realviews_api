c0e669b0544a961a4827caa4e8f3aab568650b81
pradeep-r-0 <pradeep.repudi@gmail.com>
2025-11-11 01:15:42 +0530

    order by odometer reading for recent topups

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -4,7 +4,7 @@ module CarsHelper
     
     now = fuel_topups.first
     previous = fuel_topups.last
-    return -1 unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
+    return "Not available" unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
     
     (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/(fuel_topups.pluck(:price) - [now.price]).sum.to_f)*previous.rate_per_litre.to_f).round(2)
   end
