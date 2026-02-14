5924fb05ed912e694cc5784534c4051f9c7c90ad
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-02-15 02:32:47 +0530

    fuel_topus card view

--- app/models/fuel_topup.rb
+++ app/models/fuel_topup.rb
@@ -5,4 +5,15 @@ class FuelTopup < ApplicationRecord
   belongs_to :car
 
   validates :price, :topup_date, presence: true
+
+  def mileage_from(next_topup)
+    return nil unless next_topup && odometer_reading && next_topup.odometer_reading
+    mileage = calculate_mileage(next_topup)
+    (mileage > 30 || mileage < 0) ? nil : mileage
+  end
+
+  private
+  def calculate_mileage(next_topup)
+    (((next_topup.odometer_reading.to_i - odometer_reading.to_i)/price.to_f)*rate_per_litre.to_f).round(2)
+  end
 end
