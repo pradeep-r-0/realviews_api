6d38feae26b2fa4379858d20f4350f838175cf3a
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-04-20 01:20:59 +0530

    removed attachment in mailer

--- app/mailers/e20_mailer.rb
+++ app/mailers/e20_mailer.rb
@@ -3,12 +3,7 @@ class E20Mailer < ApplicationMailer
 
   def send_mail(user)
     @user = user
-    file = Rails.root.join("app/assets/images/e20_feature.png")
 
-    attachments.inline['e20_feature.png'] = {
-      mime_type: 'image/png',
-      content: File.binread(file)
-    }
     mail(
       to: @user.email,
       subject: "New Feature: Track Your E20 Fuel Stats 🚗⚡"
