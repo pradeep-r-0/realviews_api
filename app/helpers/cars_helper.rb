dab12e1790d06fa737d323253a4551816511bf97
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-04-19 13:06:06 +0530

    E20 mileage block

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -8,4 +8,29 @@ module CarsHelper
 
     (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/(fuel_topups.pluck(:price) - [ now.price ]).sum.to_f)*previous.rate_per_litre.to_f).round(2)
   end
+
+  def formatted_mileage(mileage:, is_latest:, is_e20:)
+    return burning_now_html if is_latest
+    return content_tag(:span, "-", class: "mileage-status") if mileage.nil?
+
+    content_tag(:span, class: "mileage-main") do
+      concat content_tag(:span, mileage, class: "mileage-value")
+
+      if is_e20
+        concat content_tag(:span, "E20", class: "e20-badge"){
+          image_tag("e20.png", class: "e20-logo") +
+          content_tag(:span, "E20", class: "e20-text")
+        }
+      end
+    end
+  end
+
+  private
+
+  def burning_now_html
+    content_tag(:span) do
+      content_tag(:i, "", class: "fas fa-fire-alt", style: "color:orange") +
+      " Burning now"
+    end
+  end
 end
