3646c027738abdbc8771b3d90bbf9652b9986872
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-04-19 15:38:16 +0530

    avg mileage E20 and before

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,13 +1,27 @@
 module CarsHelper
-  def calculate_avg_mileage(fuel_topups)
+  def calculate_avg_mileage(fuel_topups, original_topups:)
     return -1 unless fuel_topups.size > 1
 
     now = fuel_topups.first
     previous = fuel_topups.last
-    return "Not available" unless now.odometer_reading || previous.odometer_reading || !previous.price || !previous.rate_per_litre
 
-    (((now.odometer_reading.to_i - previous.odometer_reading.to_i)/(fuel_topups.pluck(:price) - [ now.price ]).sum.to_f)*previous.rate_per_litre.to_f).round(2)
+    return "Not available" unless now.odometer_reading &&
+                                  previous.odometer_reading &&
+                                  previous.price &&
+                                  previous.rate_per_litre
+
+    total_price = fuel_topups.pluck(:price).sum
+
+    # 🔥 subtract only if this is the "latest dataset"
+    if fuel_topups.first == original_topups.first
+      total_price -= now.price
+    end
+
+    distance = now.odometer_reading.to_i - previous.odometer_reading.to_i
+
+    ((distance / total_price.to_f) * previous.rate_per_litre.to_f).round(2)
   end
+
 
   def formatted_mileage(mileage:, is_latest:, is_e20:)
     return burning_now_html if is_latest
