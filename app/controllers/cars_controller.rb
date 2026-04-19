f997f2ef97234babb111a6352317381755361746
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-04-20 01:39:02 +0530

    safe handle latest_topup

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -20,7 +20,7 @@ class CarsController < ApplicationController
     @fuel_topups = car.fuel_topups.where.not(odometer_reading: nil).order(topup_date: :desc, odometer_reading: :desc).limit(5)
     @e20_topups = car.fuel_topups.where.not(odometer_reading: nil).where("topup_date >= '2026-04-01'").order(topup_date: :desc, id: :desc).select(:id, :odometer_reading, :price, :rate_per_litre, :topup_date)
     @non_e20_topups = car.fuel_topups.where.not(odometer_reading: nil).where("topup_date < '2026-04-01'").order(topup_date: :desc, id: :desc).select(:id, :odometer_reading, :price, :rate_per_litre, :topup_date)
-    @latest_topup_id = @fuel_topups.first.id
+    @latest_topup_id = @fuel_topups.first&.id
     @ownership = car.ownerships.find_by_user_id(current_user.id) if current_user
   end
 
