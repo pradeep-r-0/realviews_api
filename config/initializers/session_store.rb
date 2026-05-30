cbd861f6a2d4a5a58d857fc8f967c7b5b8136224
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-05-30 12:29:41 +0530

    store session for 7 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
   key: '_realviews_session',
-  expire_after: 24.hours,
+  expire_after: 7.days,
   secure: Rails.env.production?,
   same_site: :lax
