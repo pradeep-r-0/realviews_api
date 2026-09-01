12ced71de31845ef6f3a744c79a75ac23b417abd
pradeep-r-0 <pradeep.repudi@gmail.com>
2026-09-01 23:35:11 +0530

    show/edit undeleted expenses only

--- app/controllers/balance_sheets_controller.rb
+++ app/controllers/balance_sheets_controller.rb
@@ -39,7 +39,7 @@ class BalanceSheetsController < ApplicationController
   # GET /balance_sheets/:id/edit
   def edit
     @balance_sheet = current_user.balance_sheets.find(params[:id])
-    @expenses = @balance_sheet.expenses
+    @expenses = @balance_sheet.expenses.undeleted
     # Build an expense if none exist yet
     #@balance_sheet.expenses.build if @expenses.empty?
   end
